94389da4249bc6c5d3ba11b865d78e800dbb2b78
H.İbrahim KURT <halilibrahimkurtpromed@gmail.com>
2023-02-28 21:36:33 +0300

    PatikaSQL9
    city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
    customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
    customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.

---
+++ SQL9.sql
@@ -0,0 +1,8 @@
+--SELECT city.city,country.country FROM city
+--INNER JOIN country ON country.country_id = city.country_id;
+
+--SELECT payment.payment_Id,customer.first_name,customer.last_name FROM payment
+--INNER JOIN customer ON payment.customer_Id=customer.customer_Id;
+
+--SELECT rental.rental_Id,customer.first_name,customer.last_name FROM rental 
+--INNER JOIN customer ON rental.customer_Id=customer.customer_Id;
